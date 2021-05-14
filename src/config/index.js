917fc6a44e453888c6e814e05db3357528bcbcc6
sjh <jiu.hong.sun@gmail.com>
2021-05-14 17:20:35 +0800

    upload config

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const AccessToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWEwNTNlMjdlYzc4ODYzZTA3ZGQ2OWY1ZDg2OGUxMyIsInN1YiI6IjYwN2MwOWI1NzEzZWQ0MDA3ZDBhZWY2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eGGWaw9V_pY7-78v4CTj9BG88lAaEykCBRDV_PuwksU";
+
+export const imgBaseUrl = "https://image.tmdb.org/t/p";
